f0171846bce5f58e5d420ce7e1f16c4ee88be7e3
dhika9050 <96412333+dhika9050@users.noreply.github.com>
2021-12-21 10:41:47 +0700

    Adding Enemy
    Adding Declaration of Enemy

---
+++ Assets/HealthPoint.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPoint : MonoBehaviour
+{
+	private EnemyAnimator enemny_anim;
+	private NavMeshAgent navAgent;
+	private EnemyController enemy_Controller;
+
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
